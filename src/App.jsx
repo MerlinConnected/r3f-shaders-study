0a51da87f573f11c8b386fbada8b1bb6c513e96c
MerlinConnected <gaetanjestin@outlook.com>
2023-02-10 21:24:55 +0100

    getting wavy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { useRef, useMemo } from 'react';
-import { Color } from 'three';
 
 import vertexShader from './vertexShader';
 import fragmentShader from './fragmentShader';
@@ -9,12 +8,27 @@ import fragmentShader from './fragmentShader';
 const Flag = () => {
   const mesh = useRef();
 
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0
+      }
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+  });
+
   return (
     <mesh ref={mesh} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
       <planeGeometry args={[1, 1, 32, 32]} />
       <shaderMaterial
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
+        uniforms={uniforms}
       />
     </mesh>
   );
